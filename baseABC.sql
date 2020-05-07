4eede201fe7727750e3030c7ec16a25cd0597046
EmmanueL1201 <53243889+EmmanueL1201@users.noreply.github.com>
2020-05-07 18:12:58 -0500

    Add files via upload

---
+++ baseABC.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.8-MariaDB : Database - abc
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`abc` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `abc`;
+
+/*Table structure for table `tbl_catalogo` */
+
+DROP TABLE IF EXISTS `tbl_catalogo`;
+
+CREATE TABLE `tbl_catalogo` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `correo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `activo` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `tbl_catalogo` */
+
+insert  into `tbl_catalogo`(`id`,`nombre`,`telefono`,`correo`,`activo`,`created_at`,`updated_at`) values (2,'eliotte','7224254658','eliot12@gmail.com',1,NULL,'2020-05-07 22:14:39'),(3,'emmanuel','7227625679','emmanuel@gmail.com',1,NULL,NULL),(4,'Loreli','7223456789','lorelia@gmail.com',1,'2020-05-07 19:59:44','2020-05-07 19:59:44');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
